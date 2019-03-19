f1207ca0cbb9c2fcca8b3cce5d7a2a1879283035
sunreet singh <z5130780@student.unsw.edua.au>
2019-03-19 15:32:35 +1100

    US based outbreaks able to print and store

--- webscraper.py
+++ webscraper.py
@@ -10,9 +10,12 @@ response = requests.get(url)
 content = BeautifulSoup(response.content, "html.parser")
 
 OutbreakArr = []
-for outbreaks in content.findAll('div', attrs={"class": "card-body bg-tertiary"}):
+usBasedBox = content.find('div', attrs={"class": "card-body bg-tertiary"})
+bulletPoints = usBasedBox.find('ul', attrs={"class": "list-bullet feed-item-list"})
+
+for outbreaks in bulletPoints.findAll('li'):
     outbreakObject = {
-        "url": url,
+        "url": outbreaks.find("a").get("href"),
         "date_of_publication": outbreaks.find('span', attrs={"class": "item-pubdate"}).text.encode('utf-8'),
         "headline": outbreaks.find('a', attrs={"class": "feed-item-title"}).text.encode('utf-8'),
         #"main_text": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
@@ -29,3 +32,4 @@ for i in jsonData:
     print i['url']
     print i['date_of_publication']
     print i['headline']
+    print "\n"
