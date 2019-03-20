fcf96dfee2d8be629867b1b092b01007419375c5
sunreet singh <z5130780@student.unsw.edua.au>
2019-03-20 18:27:44 +1100

    able to fetch links in webpage

--- webscraper.py
+++ webscraper.py
@@ -16,37 +16,44 @@ OutbreakArr = []
 usBasedBox = content.find('div', attrs={"class": "card-body bg-tertiary"})
 bulletPoints = usBasedBox.find('ul', attrs={"class": "list-bullet feed-item-list"})
 
+pattern = re.compile("^https")
+
+
 for outbreaks in bulletPoints.findAll('li'):
     urls = outbreaks.find('a')
-    print urls.get('href')
-    #: re.compile("^http://")})
-    if urls != None and "https" in urls:
+
+    if urls != None: #and "https" in urls:
         url4newpage = urls.get('href')
-        print url4newpage
-        newPageContent = fetchNewPages(url4newpage)
+        if pattern.match(url4newpage):
+            #print url4newpage
+            newPageContent = fetchNewPages(url4newpage)
+            newPageContentTitle = newPageContent.find("button", attrs={"id": "sectionNavButton"})
+            print newPageContentTitle
 
-        outbreakObject = {
+            ans = newPageContentTitle.find("span", attrs={"class": "mobile-title"}).text.encode('utf-8')
+            print ans
+            outbreakObject = {
 
-            "url": outbreaks.find("a").get("href"),
-            #url1 = outbreaks['href'],
-            #url1 = outbreaks.find("a").get("href"),
-            #"url": url,
-            "title": newPageContent.find('nav', attrs={"id": "mobilenav"}).text.encode('utf-8'),
-            #"date_of_publication": outbreaks.find('span', attrs={"class": "item-pubdate"}).text.encode('utf-8'),
-            #"headline": outbreaks.find('a', attrs={"class": "feed-item-title"}).text.encode('utf-8'),
-            #"main_text": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
-            #"reports": tweet.find('p', attrs={"class": "shares"}).text.encode('utf-8')
+                "url": url4newpage,
+                #url1 = outbreaks['href'],
+                #url1 = outbreaks.find("a").get("href"),
+                #"url": url,
+                #"title": newPageContentTitle.find("span", attrs={"class": "mobile-title"}).text.encode('utf-8'),
+                #"date_of_publication": outbreaks.find('span', attrs={"class": "item-pubdate"}).text.encode('utf-8'),
+                #"headline": outbreaks.find('a', attrs={"class": "feed-item-title"}).text.encode('utf-8'),
+                #"main_text": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
+                #"reports": tweet.find('p', attrs={"class": "shares"}).text.encode('utf-8')
             }
-        OutbreakArr.append(outbreakObject)
-    with open('data.json', 'w') as outfile:
-        json.dump(OutbreakArr, outfile)
+            OutbreakArr.append(outbreakObject)
+        with open('data.json', 'w') as outfile:
+            json.dump(OutbreakArr, outfile)
 
-    with open('data.json') as json_data:
-        jsonData = json.load(json_data)
+        with open('data.json') as json_data:
+            jsonData = json.load(json_data)
 
 for i in jsonData:
     print i['url']
-    print i['title']
+    #print i['title']
     #print i['date_of_publication']
     #print i['headline']
     print "\n"
