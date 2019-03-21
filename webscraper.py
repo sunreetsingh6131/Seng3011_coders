0033608481518e8078bad2050082bbcb5f2fe509
sunreet singh <z5130780@student.unsw.edua.au>
2019-03-21 13:53:38 +1100

    headline and URL done

--- webscraper.py
+++ webscraper.py
@@ -4,6 +4,21 @@ import json
 import re
 
 
+
+# class Student(object):
+#     name = ""
+#     age = 0
+#     major = ""
+#
+# def make_student(name, age, major):
+#     student = Student()
+#     student.name = name
+#     student.age = age
+#     student.major = major
+#     # Note: I didn't need to create a variable in the class definition before doing this.
+#     student.gpa = float(4.0)
+#     return student
+
 def fetchNewPages(url):
     #url = 'https://www.cdc.gov/outbreaks/'
     response = requests.get(url)
@@ -27,33 +42,43 @@ for outbreaks in bulletPoints.findAll('li'):
         if pattern.match(url4newpage):
             #print url4newpage
             newPageContent = fetchNewPages(url4newpage)
-            newPageContentTitle = newPageContent.find("button", attrs={"id": "sectionNavButton"})
-            print newPageContentTitle
 
-            ans = newPageContentTitle.find("span", attrs={"class": "mobile-title"}).text.encode('utf-8')
+            #checks if the link is none
+            ans = newPageContent.find("h1", attrs={"id": "content"})
             print ans
+
+            if ans != None:
+                headline = newPageContent.find("h1", attrs={"id": "content"}).text.encode('utf-8')
+            else:
+                headline = "unknown"
+
             outbreakObject = {
 
                 "url": url4newpage,
-                #url1 = outbreaks['href'],
-                #url1 = outbreaks.find("a").get("href"),
-                #"url": url,
-                #"title": newPageContentTitle.find("span", attrs={"class": "mobile-title"}).text.encode('utf-8'),
+                "headline": headline,
                 #"date_of_publication": outbreaks.find('span', attrs={"class": "item-pubdate"}).text.encode('utf-8'),
                 #"headline": outbreaks.find('a', attrs={"class": "feed-item-title"}).text.encode('utf-8'),
                 #"main_text": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
                 #"reports": tweet.find('p', attrs={"class": "shares"}).text.encode('utf-8')
+
+                # {"reports": [
+                #     {   "diseases": 100,
+                #         "syndrome": "reception",
+                #         "reported_events": 50,
+                #         "comments": 75
+                #     },
+                # ]},
             }
             OutbreakArr.append(outbreakObject)
-        with open('data.json', 'w') as outfile:
-            json.dump(OutbreakArr, outfile)
+            with open('data.json', 'w') as outfile:
+                json.dump(OutbreakArr, outfile)
 
-        with open('data.json') as json_data:
-            jsonData = json.load(json_data)
+            with open('data.json') as json_data:
+                jsonData = json.load(json_data)
 
 for i in jsonData:
     print i['url']
-    #print i['title']
+    print i['headline']
     #print i['date_of_publication']
     #print i['headline']
     print "\n"
