9ab80a7fc84efecff69878a2de5e5543359a127b
Amol Jain <amol@Amols-MBP.hub>
2019-03-25 12:06:54 +1100

    Updating with master

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # SENG3011_Coders
 Set up the gitub repo.
 
-The Management info and Design details have been uploaded for deliverable 1.
-Team will now start working on coding part for deliverabe 2.
+The Management info and Design details have been uploaded for deliverable 1. Team will now start working on coding part for deliverabe 2.
 
-Run the Webserver.py as python Webserver.py PORTNUMBER
-and in the http://127.0.0.1:PORTNUMBER/index.html to display the content of the html file.
+Webserver.py :
+
+Run the Webserver.py as "python Webserver.py PORTNUMBER" and in the http://127.0.0.1:PORTNUMBER/index.html to display the content of the html file.
+
+Try and run the Webserver.py at a different port everytime to avoid the message "Port number already in use!"
+
+webscraper.py :
+
+API online url - Base URL: virtserver.swaggerhub.com/samantha-p/cdc-scraper/1.0.0
--- WebServer.py
+++ WebServer.py
@@ -1,39 +1,203 @@
-#!/usr/bin/python
+<!-- insert map -->
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<style>
+body {margin:0;height:0;}
+* {box-sizing: border-box;}
+.open-button {
+  background-color: #4169e1;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+  position: fixed;
+  bottom: 23px;
+  right: 28px;
+  width: 250px;
+}
+/* The popup chat - hidden by default */
+.chat-popup {
+  display: none;
+  position: fixed;
+  bottom: 0;
+  right: 15px;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+/* Full-width textarea */
+.form-container textarea {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+  resize: none;
+  min-height: 200px;
+}
+/* When the textarea gets focus, do something */
+.form-container textarea:focus {
+  background-color: #ddd;
+  outline: none;
+}
+/* Set a style for the submit/send button */
+.form-container .btn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: #4169e1;
+}
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .open-button:hover {
+  opacity: 1;
+}
+.navbar {
+    margin-bottom: 0;
+    border-radius: 0;
+}
+.icon-bar {
+  position: fixed;
+  top: 50%;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+.icon-bar a {
+  display: block;
+  text-align: center;
+  padding: 16px;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 20px;
+}
+.icon-bar a:hover {
+  background-color: #000;
+}
+.facebook {
+  background: #3B5998;
+  color: white;
+}
+.twitter {
+  background: #55ACEE;
+  color: white;
+}
+.google {
+  background: #dd4b39;
+  color: white;
+}
+.linkedin {
+  background: #007bb5;
+  color: white;
+}
+.youtube {
+  background: #bb0000;
+  color: white;
+}
+.content {
+  margin-left: 75px;
+  font-size: 30px;
+}
+</style>
 
-import socket
-import sys
-from socket import *
-import argparse
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">HEALTHMAP</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+    </div>
+</div>
+</nav>
 
-Host = ''
-p = argparse.ArgumentParser()
-p.add_argument("Port", type=int)
-args = p.parse_args()
-Port = args.Port
-Socket = socket(AF_INET, SOCK_STREAM)
-Socket.bind((Host, Port))
-Socket.listen(1)
+<body>
+<p style="margin-top: 1em"> <font size="5" face="sans-serif" color="red"><marquee>All the alerts will go here</marquee>
+</body>
 
-if len(sys.argv) != 2:
-    print 'Usage: Invalid number of parameters'
-    sys.exit();
+<!-- using a responsive iframe -->
+<div class="responsive-map-container">
+  <!-- place the iframe code between here... -->
+    <iframe src="https://www.google.com/maps/embed" width="600" height="450" frameborder="0" style="border:0"></iframe>
+  <!-- ... and here -->
+</div>
 
-while True:
-	print ("Running Server")
-        conn, addr = Socket.accept()
-        try:
-                request = conn.recv(1024)
-                response = request.split()[1];
-                fileName = response.replace('/', '')
-                file = open(fileName)
-                output = file.read()
-                conn.send('HTTP/1.1 200 OK\n\n')
-                conn.send(output)
-                conn.close()
-        except IOError:
-                conn.send('HTTP/1.1 404 File not found\n\n')
-                conn.send('404 Error: File not found')
-                conn.close()
+<div class="icon-bar">
+  <a href="https://m.facebook.com/CDC/?__tn__=CH-R" class="facebook"><i class="fa fa-facebook"></i></a>
+  <a href="https://www.cdc.gov/socialmedia/tools/Twitter.html" class="twitter"><i class="fa fa-twitter"></i></a>
+  <a href="https://www.cdc.gov/" class="google"><i class="fa fa-google"></i></a>
+  <a href="https://au.linkedin.com/company/centers-for-disease-control-and-prevention" class="linkedin"><i class="fa fa-linkedin"></i></a>
+</div>
 
+<button class="open-button" onclick="openForm()">Chat</button>
 
+<div class="chat-popup" id="myForm">
+    <form action="/action_page.php" class="form-container">
+    <label for="msg"><b>Message</b></label>
+    <textarea placeholder="Type message.." name="msg" required></textarea>
+    <button type="submit" class="btn">Send</button>
+    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+    </form>
+</div>
 
+<!-- /insert map -->
+<style media="screen">
+.responsive-map-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 40px;
+    height: 0;
+    overflow: hidden;
+}
+.responsive-map-container iframe,
+.responsive-map-container object,
+.responsive-map-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
+
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+</script>
+
+</body>
+</html>
