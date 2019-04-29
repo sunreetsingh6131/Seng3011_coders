a8375f9e41aa3201c43c81b445d0196efda96ee8
bhawnakundu <38641482+bhawnakundu@users.noreply.github.com>
2019-04-29 18:17:34 +1000

    Update index.py

--- PHASE_2/Application_SourceCode/index.py
+++ PHASE_2/Application_SourceCode/index.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, Response, jsonify
-#from newsapi import NewsApiClient
-#84ceb92e06b44f1db554d716c0fa0a01 API KEY
 from flask_restplus import Api, Resource , fields
 from flask_cors import CORS
 import datetime , re
@@ -52,9 +50,6 @@ def checkDate(dates):
         return False
     return True
 
-
-
-
 # function used to split a date strting
 def getDateInParts(inputs):
     inputs = make_string(inputs)
@@ -100,52 +95,19 @@ ns = api.namespace('outbreaktable', description='Returns the analysis after gett
 @api.doc(params={'location': 'example: location = New York'})
 @api.doc(params={'end_date': 'end of period of interest. example: 2019-02-25T03:00:00'}, required=True)
 @api.doc(params={'start_date': 'start of period of interest. example: 2019-02-15T03:00:00'}, required=True)
-
-
-# @ns.route('/show/<string:start_date>/<string:end_date>/<string:location>/<string:key_terms>')
-# #@cross_origin()
-#
-# @ns.route('/show/<string:start_date>/<string:end_date>/<string:key_terms>')
-# #@cross_origin()
-# @ns.route('/show/<string:start_date>/<string:end_date>/<string:location>')
-# #@cross_origin()
-#
-# @ns.route('/show/<string:start_date>/<string:end_date>')
-#@cross_origin()
 @api.response(404, 'database not found.')
 @api.response(400, 'Invalid inputs.')
-
-# @app.route('/show')
-# def query_example():
-#     keyTerm = request.args.get('keyterm') #if key doesn't exist, returns None
-#     location = request.args['location'] #if key doesn't exist, returns a 400, bad request error
-#     startDate = request.args.get('start_date')
-#     endDate = request.args.get('end_date')
-# print request.args
-# >> {"arg1": "hello", "arg2": "world"}
 
 class show(Resource):
 
     @api.response(200, 'Data found and analysis shown.')
     def get(self):
 
-        #location = request.args.['location'] #if key doesn't exist, returns a 400, bad request error
         key_terms = request.args.get('keyterm') #if key doesn't exist, returns None
         location = request.args.get('location') #if key doesn't exist, returns a 400, bad request error
         start_date = request.args.get('start_date')
-        # if start_date == None:
-        #     res = {
-        #          "details": "Field can't be left empty",
-        #     }
-        #     return res, status.HTTP_400_BAD_REQUEST
         end_date = request.args.get('end_date')
-        # if end_date == None:
-        #     res = {
-        #          "details": "Field can't be left empty",
-        #     }
-        #     return res, status.HTTP_400_BAD_REQUEST
-        #  if location != all then check the validity of the location
-
+        
         if location != None:
             #print("here")
             if location != 'all':
@@ -197,18 +159,6 @@ class show(Resource):
             e_date = datetime.datetime(int(e_year), int(e_month), int(e_day), int(e_hour), int(e_min), int(e_sec))
             ce_date = datetime.datetime(int(e_year), int(e_month), int(e_day))
 
-        # Checking is the start_date is less than the end_date
-        #console.log(e_date)
-
-        # print(s_date)
-        # print(e_date)
-
-        # if start_date == None or end_date == None:
-        #     res = {
-        #          "details": "date fields can't be left empty",
-        #     }
-        #     return res , status.HTTP_400_BAD_REQUEST
-
         if start_date !=None or end_date != None:
             if cs_date > ce_date :
                 res = {
@@ -224,7 +174,6 @@ class show(Resource):
         data=[]
         for row in result_rows:
             # For each word in key term check if it is in title/headline
-
             #  we need to split the key terms and store it in the array
             if key_terms != None:
                 key_terms = make_string(key_terms)
@@ -237,12 +186,9 @@ class show(Resource):
 
                 isSubstring = False;
                 for word in key_terms1:
-                    # print("my input ->"+word)
-                    # print("row in database-> "+row[11])
                     if word.lower() in row[11].lower():
                         isSubstring = True;
-                        # print("yesyesyes")
-                        break #break
+                        break
 
                 # code below unreachable not using keyterms always.
                 if isSubstring == False:
@@ -276,14 +222,10 @@ class show(Resource):
                 if isIt is False:
                     continue
 
-            # type1 = None
-            # type2 = None
-            # type3 = None
-
             reportedArr = []
             type = [None, None, None]
             no = [0, 0, 0]
-            # print(row[6])
+            
             if row[5] != 'unknown':
                 if row[5] != None:
                     if int(row[5]) > 0:
@@ -309,12 +251,6 @@ class show(Resource):
                         'number_affected': "unknown"
                     }
                     reportedArr.append(reported)
-
-
-            # 'hospitalised' : row[6],
-            # 'deaths' : row[7],
-            # 'locations' : row[8]
-
 
             i = 0
             while i < 3:
@@ -351,23 +287,11 @@ class show(Resource):
                 "date_of_publication": row[3],
                 "main_text": row[4],
                 'reports' : [report]
-
-
             }
 
-            # Fill dictionary with items
-            # item = json.dumps(item)
-            # data = data + str(item)
+            # Fill dictionary with items          
             data.append(item)
-            # for x in data:
-            #     print(x)
-
-            # print(item)
-            #print('\n'.join(data))
-            #print(data)
-            #json_data = json.dumps(data)
-
-
+            
         datax = []
         if data == datax:
             return {
